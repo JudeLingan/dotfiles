a7a44ebb8497430070777f4522166a6fd9c4db14
Jude Lingan <judecohenlingan@gmail.com>
2024-11-21 15:22:07 -0500

    auto-lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -8,9 +8,7 @@ require('mason').setup({
     }
 })
 
-require('mason-lspconfig').setup()
-
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").rust_analyzer.setup {}
-require("lspconfig").jdtls.setup {}
-require("lspconfig").lemminx.setup {}
+require('mason-lspconfig').setup {
+    ensure_installed =  { "lua_ls", "rust_analyzer", "jdtls" },
+    automatic_installation = true,
+}
