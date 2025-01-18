ee4622f51fa310c45f929f8bc76549ebacc7902a
Jude Lingan <judecohenlingan@gmail.com>
2025-01-18 14:02:37 -0500

    Changed from spaces to tabs Added godot lsp to required

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,10 +1,10 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
     -- ensure_installed = {},
-    automatic_installation = false,
+    automatic_installation = true,
 }
 
 require('lspconfig').gleam.setup({})
-require('lspconfig').ocamllsp.setup({})
 require('lspconfig').rust_analyzer.setup({})
 require('lspconfig').jdtls.setup({})
+require('lspconfig').gdscript.setup{}
