fa43dd2c4d8f52194e3c6c860c2cbe6c6c7d335f
Jude Lingan <judecohenlingan@gmail.com>
2025-03-23 00:33:26 -0400

    completed moving of files

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,18 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.foldmethod = "indent"
+vim.opt.foldenable = false
+vim.opt.foldlevel = 2000
+
+vim.opt.winblend = 0
+vim.opt.cursorline = true
