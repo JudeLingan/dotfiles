eb4a7f9b2a3fbf2d238df7a0494be1f9a2d27823
Jude Lingan <judecohenlingan@gmail.com>
2025-03-23 11:04:23 -0400

    stable release of copilot-chat

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -25,10 +25,23 @@ return {
 	-- AI Integration
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
+		version = true,
+		cmd = "CopilotChat",
+		opts = function()
+			local user = vim.env.USER or "User"
+			user = user:sub(1, 1):upper() .. user:sub(2)
+			return {
+				auto_insert_mode = true,
+				question_header = "  " .. user .. " ",
+				answer_header = "  Copilot ",
+				window = {
+					width = 0.4,
+				},
+			}
+		end,
 		dependencies = {
 			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
 			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
-		build = "make tiktoken", -- Only on MacOS or Linux
 	},
 }
