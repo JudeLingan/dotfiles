8b01748a453fda9042cd84783e756d5cb2d886ad
JudeLingan <judecohenlingan@gmail.com>
2025-04-01 01:56:57 -0400

    screw barbar

--- nvim/lua/plugins/layout.lua
+++ nvim/lua/plugins/layout.lua
@@ -4,15 +4,4 @@ return {
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' }
 	},
-	{'romgrk/barbar.nvim',
-		dependencies = {
-			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-		},
-		init = function() vim.g.barbar_auto_setup = false end,
-		opts = {
-			auto_hide = 1,
-		},
-		version = '^1.0.0', -- optional: only update when a new 1.x version is released
-	},
 }
