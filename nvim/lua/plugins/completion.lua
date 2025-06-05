3511a0993ca7852267d0aca570377f202bf0c6c7
JudeLingan <judecohenlingan@gmail.com>
2025-06-05 19:02:39 -0400

    added copilot chat

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -23,23 +23,15 @@ return {
 	},
 
 	{
-		"olimorris/codecompanion.nvim",
-		config = true,
-		opts = function()
-			return {
-				strategies = {
-					chat = {
-						adapter = "copilot",
-					},
-					inline = {
-						adapter = "copilot",
-					},
-				},
-			}
-		end,
+		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
-			{"nvim-lua/plenary.nvim", branch = "master"},
-			"nvim-treesitter/nvim-treesitter",
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
 	},
 }
