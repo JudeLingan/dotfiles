15f793c7eb615fa142d67b7bb9a043adc8bd3b81
Jude Lingan <judecohenlingan@gmail.com>
2025-06-28 15:42:28 -0400

    disabled autocompletion from copilot

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -21,11 +21,16 @@ return {
 			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
 		},
 	},
+	{
+		"github/copilot.vim",
+		config = function()
+			vim.g.copilot_filetypes = { ["*"] = false, }
+		end,
+	},
 
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
-			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
 			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
 		build = "make tiktoken", -- Only on MacOS or Linux
