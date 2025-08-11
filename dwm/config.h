6c0f2ebd91ed2c4e6eb9a31402dc44d3ed09ad82
Jude Lingan <judecohenlingan@gmail.com>
2025-08-11 15:31:35 -0400

    bumped up default gap due to increased functionality

--- dwm/config.h
+++ dwm/config.h
@@ -3,7 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 8, .gappx = 8};
+static const Gap default_gap        = {.isgap = 1, .realgap = 16, .gappx = 16};
 static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
