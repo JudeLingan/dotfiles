d13941beebb30d501d9820db168ab1f6ba6fddff
Jude Lingan <judecohenlingan@gmail.com>
2025-09-11 22:57:27 -0400

    unlualined everything

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -95,12 +95,6 @@ return {
 		end,
 	},
 
-	-- Status line
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' }
-	},
-
 	-- Treesitter
 	{
 		"nvim-treesitter/nvim-treesitter",
