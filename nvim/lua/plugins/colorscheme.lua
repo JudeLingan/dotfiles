b8604b22a2172d32095722e22285ebd813bf89f5
JudeLingan <judecohenlingan@gmail.com>
2025-10-24 17:46:00 -0400

    removed pywal

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -74,15 +74,6 @@ return {
 		end,
 	},
 
-	{
-		'uZer/pywal16.nvim',
-		-- for local dev replace with:
-		-- dir = '~/your/path/pywal16.nvim',
-		config = function()
-			vim.cmd.colorscheme("pywal16")
-		end,
-	},
-
 	-- Treesitter
 	{
 		"nvim-treesitter/nvim-treesitter",
