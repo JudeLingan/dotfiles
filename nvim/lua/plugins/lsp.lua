adcae4befef68df319386be0b37ca06fcb5df581
JudeLingan <judecohenlingan@gmail.com>
2025-11-29 19:35:53 -0500

    moved config/lsp.lua to plugins/lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,24 @@
 return {
 	-- LSP SERVERS
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+
+		config = function (_, opts)
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+
+			vim.diagnostic.config({
+				underline = true,
+				virtual_text = false,
+				severity_sort = true,
+				float = {scope = 'line'}
+			})
+		end,
+	},
 
 	{
 		"mfussenegger/nvim-jdtls",
@@ -12,5 +28,36 @@ return {
 		end
 	},
 
-	"niscolas/nvim-godot",
+	{
+		"lommix/godot.nvim",
+		lazy = true,
+		cmd = { "GodotDebug", "GodotBreakAtCursor", "GodotStep", "GodotQuit", "GodotContinue" },
+		opts = {
+			-- Path to your Godot executable
+			bin = "godot",
+
+			-- DAP configuration
+			dap = {
+				host = "127.0.0.1",
+				port = 6006,
+			},
+
+			-- GUI settings for console (passed to nvim_open_win)
+			gui = {
+				console_config = {
+					anchor = "SW",
+					border = "double",
+					col = 1,
+					height = 10,
+					relative = "editor",
+					row = 99999,
+					style = "minimal",
+					width = 99999,
+				},
+			},
+
+			-- Expose user commands automatically (optional)
+			expose_commands = true,
+		},
+	},
 }
