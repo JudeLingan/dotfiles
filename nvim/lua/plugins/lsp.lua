4f1755d4177b4ae0a0905943561724e317a14de7
Jude Lingan <judecohenlingan@gmail.com>
2025-12-05 20:21:11 -0500

    removed annoying error

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
 			vim.lsp.config.gdscript = {
 				name = "godot",
 				cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
+				filetypes = { "gd", "tscn", "gdshader" },
 			}
 			vim.lsp.enable("gdscript")
 
