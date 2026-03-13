bcb940fc3e5b1639717d71a24664237725ef16d4
Jude Lingan <judecohenlingan@gmail.com>
2026-03-13 13:19:15 -0400

    added buffer hotkeys

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,6 +12,10 @@ vim.keymap.set('n', '<leader>fr', scope.lsp_references, { desc = 'Telescope LSP 
 vim.keymap.set('n', '<leader>tc', vim.cmd.tabnew)
 vim.keymap.set('n', '<leader>tx', vim.cmd.tabclose)
 
+--buffers
+vim.keymap.set('n', '<leader>bp', vim.cmd.bprev)
+vim.keymap.set('n', '<leader>bn', vim.cmd.bnext)
+
 -- diagnostics
 vim.keymap.set('n', '<leader>ds', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>df', vim.lsp.buf.code_action)
