74d2ba52a47f180361af8c82d99499e5807c1967
Jude Lingan <judecohenlingan@gmail.com>
2026-05-17 20:00:08 -0400

    removed lsp declarations that should be in local

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,9 +14,6 @@ vim.lsp.config('*', {
 	capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
 
-vim.lsp.enable("gdscript")
-vim.lsp.enable("ts_ls")
-
 vim.diagnostic.config({
 	underline = true,
 	virtual_text = false,
